8ee77245bbcda0875311774d20dfefb394fc6f5b
NidhitheCoder <nidhpv@gmail.com>
2021-03-22 23:46:39 +0530

    create basic element file

---
+++ redux-app/src/ReduxDemo.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { createStore } from "redux";
+
+class ReduxDemo extends Component {
+  render() {
+    return <div>Test</div>;
+  }
+}
+
+export default ReduxDemo;
